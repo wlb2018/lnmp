1712de2f3a5cf887b53a0798f31275935ed3fc2b
wlb2018 <42597781+wlb2018@users.noreply.github.com>
2021-07-23 07:43:44 +0800

    Update init.sh
    修正找不到remi源的问题

--- init.sh
+++ init.sh
@@ -56,8 +56,10 @@ yum --exclude=kernel* -y update
 
 #从remi源安装最新版php、redis等
 yum -y install epel-release
-yum -y install centos-release-scl
 yum -y install remi-release
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-${releasever}.rpm
+yum -y install centos-release-scl
+
 
 yum -y install yum-utils
 
